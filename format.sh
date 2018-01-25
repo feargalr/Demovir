069ebe8d6f4288b8ab34d0c1ee9cd18516d83efa
feargalr <feargalr@gmail.com>
2018-01-25 16:23:33 +0000

    Add files via upload

--- format.sh
+++ format.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 gunzip unipro*fasta.gz
 sed 's/tr|//' unipro*fasta | sed 's/|/\t/' | cut -f 1 > uniprot_viral.sedded.fasta
-zcat TrEMBL_ids_which_have_order_family.txt.gz | pullseq -N -i uniprot_viral.sedded.fasta
+zcat TrEMBL_ids_which_have_order_family.txt.gz | pullseq -N -i uniprot_viral.sedded.fasta > uniprot_viral.formatted.fasta 
 rm uniprot_viral.sedded.fasta
-usearch -makeudb_ublast uniprot_viral.sedded.fasta  -output uniprot_trembl.viral.udb
+usearch -makeudb_ublast uniprot_viral.formatted.fasta  -output uniprot_trembl.viral.udb
+rm uniprot_viral.sedded.fasta
