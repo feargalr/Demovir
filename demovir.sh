b1d512cc4a9f30c633c59a6e6a9a5dcceba9e4b8
feargalr <feargalr@gmail.com>
2018-02-09 11:06:40 +0000

    Update demovir.sh

--- demovir.sh
+++ demovir.sh
@@ -7,7 +7,6 @@
 
 #This script assumes that gene name is contigid_geneNo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "$DIR"
 usearch -ublast $1 -db $DIR/uniprot_trembl.viral.udb -evalue $2 -blast6out trembl_ublast.viral.txt -threads $3
 sort -u -k1,1 trembl_ublast.viral.txt > trembl_ublast.viral.u.txt 
 cut -f 1,2 trembl_ublast.viral.u.txt | sed 's/_[0-9]\+\t/\t/' | cut -f 1 | paste trembl_ublast.viral.u.txt - > trembl_ublast.viral.u.contigID.txt
