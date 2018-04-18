a605f5a06ce4c0825f547029cce56e16b8cd1a9e
feargalr <feargalr@gmail.com>
2018-04-18 15:32:36 +0100

    Update demovir.sh

--- demovir.sh
+++ demovir.sh
@@ -10,6 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 usearch -ublast $1 -db $DIR/uniprot_trembl.viral.udb -evalue $2 -blast6out trembl_ublast.viral.txt -threads $3
 sort -u -k1,1 trembl_ublast.viral.txt > trembl_ublast.viral.u.txt 
 cut -f 1,2 trembl_ublast.viral.u.txt | sed 's/_[0-9]\+\t/\t/' | cut -f 1 | paste trembl_ublast.viral.u.txt - > trembl_ublast.viral.u.contigID.txt
-rm trembl_ublast.viral.u.txt
+rm trembl_ublast.viral.u.txt trembl_ublast.viral.txt
 Rscript $DIR/demovir.R
+rm trembl_ublast.viral.u.contigID.txt
 
